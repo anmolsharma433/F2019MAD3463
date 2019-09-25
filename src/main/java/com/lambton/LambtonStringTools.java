01644d2933292000e15997862a0fa22f6fc2a3a0
Anmol <anmolsharma433@gmail.com>
2019-09-25 18:30:58 -0400

    reverse function is created

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -1,4 +1,20 @@
 package com.lambton;
 
-public class LambtonStringTools {
+public class LambtonStringTools
+{
+    public String reverse(String s)
+    {
+
+        char s1[] = s.toCharArray();
+        String store;
+        char reverse[] =new char[s.length()];
+        int count = 0;
+        for(int i = s.length(); i>=0;i--)
+        {
+            reverse[count++]=s1[i];
+
+        }
+        String finalrev= new String(reverse);
+        return finalrev;
+    }
 }
--- src/main/java/com/lambton/MainClass.java
+++ src/main/java/com/lambton/MainClass.java
@@ -1,5 +1,9 @@
 package com.lambton;
 
 public class MainClass {
-    
+    public static  void main(String[] args)
+    {
+        LambtonStringTools l1= new LambtonStringTools();
+    }
+
 }
