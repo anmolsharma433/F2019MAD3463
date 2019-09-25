e660f447662c1a3e2a4d6cf5efbc5752b420fe01
Anmol <anmolsharma433@gmail.com>
2019-09-25 19:12:43 -0400

    binary to decimal function is created

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -1,20 +1,44 @@
 package com.lambton;
 
-public class LambtonStringTools
-{
-    public String reverse(String s)
-    {
+public class LambtonStringTools {
+    public String reverse(String s) {
+
+        //reverse function
 
         char s1[] = s.toCharArray();
         String store;
-        char reverse[] =new char[s.length()];
+        char reverse[] = new char[s.length()];
         int count = 0;
-        for(int i = s1.length-1; i>=0;i--)
-        {
-            reverse[count++]=s1[i];
+        for (int i = s1.length - 1; i >= 0; i--) {
+            reverse[count++] = s1[i];
 
         }
-        String finalrev= new String(reverse);
+        String finalrev = new String(reverse);
         return finalrev;
     }
+    //binary to decimal function
+
+    public int binarytodecimal(String s) {
+
+        // System.out.println(Integer.parseInt(s,2));
+        int value = Integer.parseInt(s);
+        int decimal = 0;
+        int i = 0;
+        while (true) {
+            if (value == 0)
+            {
+                break;
+            }
+            else
+                {
+                int temp = value % 10;
+                decimal = (int) (decimal + temp * Math.pow(2, i));
+                value = value / 10;
+                i++;
+            }
+
+        }
+        
+    }
 }
+
--- src/main/java/com/lambton/MainClass.java
+++ src/main/java/com/lambton/MainClass.java
@@ -3,8 +3,14 @@ package com.lambton;
 public class MainClass {
     public static  void main(String[] args)
     {
+        // first question
         LambtonStringTools l1= new LambtonStringTools();
         System.out.println("The reverse is : " +l1.reverse("Lambton"));
+
+        //Second question
+        System.out.println("The binary is "+l1.binarytodecimal("1000"));
+
+
     }
 
 }
